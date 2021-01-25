05123861f49daa151b66d0fd898a07690f6f4b65
evasive10 <avgjoe131@gmail.com>
2021-01-25 02:12:16 +0000

    Changed to run with argparse to add help menu

--- nmap.py
+++ nmap.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
 
-import os, sys
+import os, argparse
 from os import system
 
-ip = sys.argv[1]
+parser = argparse.ArgumentParser(description="Conduct nmap scan on all ports with version detection enabled, using the default set of nmap scripts. Configured to show only open or possibly open ports and the reason each port is set to specific state and reason each host is up or down.")
+
+parser.add_argument('IP', help="Desired IP address to perform scan on")
+
+args=parser.parse_args()
+ip = args.IP
 
 system(f"nmap -p- -sV -sC --open --reason {ip}")
